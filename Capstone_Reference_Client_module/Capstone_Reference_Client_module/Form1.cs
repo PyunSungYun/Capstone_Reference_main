309fe3d17f24f37b0c6ee9c422eecb31c2f134c0
DESKTOP-IC8IM2M\편성윤 <psy7217@naver.com>
2022-09-28 14:17:02 +0900

    create repository

---
+++ Capstone_Reference_Client_module/Capstone_Reference_Client_module/Form1.cs
@@ -0,0 +1,10 @@
+namespace Capstone_Reference_Client_module
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
